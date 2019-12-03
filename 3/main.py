90d0150287289537124fd289ed910e64e3255381
Sharu95 <sharanak@uio.no>
2019-12-03 19:48:07 +0100

    day 3

--- 3/main.py
+++ 3/main.py
@@ -1,53 +1,39 @@
-central_port = (8, 1)
-w1 = "R8,U5,L5,D3"
-w2 = "U7,R6,D4,L4"
-import numpy as np 
+def travel(directions, to=False):
+	seen, cur_x, cur_y, total_steps = [], 0, 0, 0
+	x, y = to if to else (None, None)
+	decode = { 'R': 1, 'L': -1, 'U': -1, 'D': 1 }
 
-right = lambda p, delta: (p[0] + delta, p[1])
-left = lambda p, delta: (p[0] - delta, p[1])
-down = lambda p, delta: (p[0], p[1] + delta)
-up = lambda p, delta: (p[0], p[1] - delta)
+	for instruction in directions:
+		steps, direction = int(instruction[1:]), instruction[0]
+		for _ in range(1, steps + 1):
+			has_arrived = cur_x == x and cur_y == y
+			if direction in ['R', 'L']: 
+				cur_x += decode[direction]
+			else: 
+				cur_y += decode[direction]
+			
+			total_steps += 1
+			
+			if to and has_arrived: 
+				return total_steps
+			else: 
+				seen.append((cur_x, cur_y))
+	return seen 
 
 def main(w1, w2):
+	# Part 1
+	seen_w1 = set(travel(w1))
+	seen_w2 = set(travel(w2))
+	ixns = seen_w1.intersection(seen_w2)
+	distances = map(lambda tpl: abs(tpl[0]) + abs(tpl[1]), ixns)
+	print(min(distances))
 
-	w1_path = {}
-	current_point = central_port
-	for direction in w1: 
-		d = int(direction[1:])
-		point = None
-		if 'R' in direction:
-			point = right(current_point, d)
-		elif 'U' in direction:
-			point = up(current_point, d)
-		elif 'L' in direction:
-			point = left(current_point, d)
-		elif 'D' in direction:
-			point = down(current_point, d)
-		else:
-			print('Unknown')
-		
-		w1_path[point] = True
-	
-	for direction in w2:
-		d = int(direction[1:])
-		point = None
-		if 'R' in direction:
-			point = right(current_point, d)
-		elif 'U' in direction:
-			point = up(current_point, d)
-		elif 'L' in direction:
-			point = left(current_point, d)
-		elif 'D' in direction:
-			point = down(current_point, d)
-		else:
-			print('Unknown')
-		
-		if point in w1_path:
-			print('Crosses at', point) 
+	# Part 2
+	time_delays = [ travel(w1, to=ixn) + travel(w2, to=ixn) for ixn in ixns ]
+	print(min(time_delays))
 
 if __name__ == '__main__':
 	with open('input.txt', 'r') as f:
-		_ = f.read()
-
+		w1, w2 = f.readlines()
 		w1, w2 = w1.split(','), w2.split(',')
 		main(w1, w2)
